fe590f43a3fb38de4890631a82819cb14eedf368
Rashid Mohammed Iliyas Khan <khanr7433@gmail.com>
2024-12-18 20:42:00 +0530

    add user model, database connection, and user routes

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.config.js";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
 const app = express();
+connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.json());
+
+app.use("/users", userRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
