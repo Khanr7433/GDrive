14b723b9f627460c8f2c30a401aba86b053b0597
Rashid Mohammed Iliyas Khan <khanr7433@gmail.com>
2025-08-05 16:26:26 +0530

    fix: update MongoDB connection URI and restore authentication middleware

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 function connectToDB() {
   mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.MONGODB_URI)
     .then(() => {
       console.log("Connected to DB");
     })
